d4b9a16d1eafdf6e559f3f33d4b5bfbe354d11bb
Alex <stalkerartefact@yandex.ru>
2019-01-12 23:50:06 +0300

    Lesson 12
    *src*
    - corrected MainStreams class
    
    Signed-off-by: Alex <stalkerartefact@yandex.ru>

--- src/MainStreams.java
+++ src/MainStreams.java
@@ -1,11 +1,10 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 public class MainStreams {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 3, 2, 3};
+        int[] arr1 = {1, 9, 8, 5, 3, 2, 3};
         int[] arr2 = {9, 8};
 
         System.out.println("Сумма уникальных чисел первого массива равна " + minValue(arr1));
@@ -15,13 +14,13 @@ public class MainStreams {
         System.out.println("Новый массив чисел: " + oddOrEven(list));
     }
 
-    private static int minValue(int[] values) throws NoSuchElementException {
-        return Arrays.stream(values).distinct().reduce((v1, v2) -> Integer.parseInt(v1 + "" + v2)).getAsInt();
+    private static int minValue(int[] values) {
+        return Arrays.stream(values).distinct().sorted().reduce(0, (v1, v2) -> Integer.parseInt(v1 + "" + v2));
     }
 
-    private static List<Integer> oddOrEven(List<Integer> list) throws NoSuchElementException {
-        int sum = list.stream().reduce((v1, v2) -> v1 + v2).get();
+    private static List<Integer> oddOrEven(List<Integer> list){
+        int sum = list.stream().reduce(0, (v1, v2) -> v1 + v2);
         System.out.println("Сумма всех чисел массива равна " + sum);
-        return list.stream().filter(num -> (sum % 2 == 0) == (num % 2 != 0)).collect(Collectors.toList());
+        return list.stream().filter(num -> (sum % 2 != num % 2)).collect(Collectors.toList());
     }
 }
